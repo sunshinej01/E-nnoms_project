45ed07a0bda3653328685d1ff75cf42935c04b76
saeram2000 <saeram2215@gmail.com>
2025-01-15 12:12:57 +0900

    페이지 연결 수정

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from streamlit_navigation_bar import st_navbar
 pages = {
     "Home": "home",
     "News trend": "news_trend",
-    "Market Data": "data_table",
+    "Market Data": "test_data",
 }
 
 # 세션 상태 초기화
 if "page" not in st.session_state:
-    st.session_state.page = "Home"  # 초기 페이지 설정
+    st.session_state.page = "home"  # 초기 페이지 설정
 
 # GNB 스타일 설정
 styles = {
@@ -30,16 +30,10 @@ styles = {
 # GNB 생성
 page = st_navbar(list(pages.keys()), styles=styles)
 
-# 선택된 페이지에 따라 내용 표시
-if page == "Home":
-    module = import_module(pages["Home"])
-    #st.title("Home")
-    #st.write("홈 페이지입니다.")
-elif page == "News trend":
-    # news.py의 run_streamlit() 호출
-    st.title("Home")
-    st.write("홈 페이지입니다.")
 
-elif page == "Market Data":
-    st.title("Market Data")
-    st.write("마켓 데이터를 준비 중입니다.")
+# 선택된 페이지에 따라 내용 표시
+if page in pages:  # 선택된 페이지가 pages 딕셔너리에 있는지 확인
+    current_page = pages[page]  # 딕셔너리에서 모듈 이름 가져오기
+    module = import_module(current_page)  # 모듈 import
+    module.render_page()  # render_page 함수 실행 (각 페이지 모듈에 정의)
+
